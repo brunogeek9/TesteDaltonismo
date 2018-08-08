90a37ca5fcbde445c95794e79a865a8d62c3cfdb
brunogeek9 <brunogeek9@gmail.com>
2018-08-08 09:55:30 -0300

    começando a brincadeira

--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/MainActivity.java
@@ -3,10 +3,12 @@ package br.ufrn.eaj.tads.testedaltonismo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int teste1 = 11,teste2 = 22, teste3 = 33;
+    public static final int TESTE1 = 11,TESTE2 = 22, TESTE3 = 33,CONF = 44, CANC = 55;
     public void sendToTeste(int n){
         Intent i = new Intent(this,TesteActivity.class);
         startActivityForResult(i,n);
@@ -24,5 +26,32 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openTeste3(View v){
         sendToTeste(33);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        int resultado = 0;
+        if (data != null){
+            Bundle dados = data.getExtras();
+            resultado = dados.getInt("resultado");
+        }
+        if(requestCode == TESTE1){
+            if(resultCode == CONF){
+                TextView tv = findViewById(R.id.tv_r1);
+                tv.setText(""+resultado);
+            }
+        }else if (requestCode == TESTE2){
+            if(resultCode == CONF){
+                TextView tv = findViewById(R.id.tv_r2);
+                tv.setText(""+resultado);
+            }
+        }else if (requestCode == TESTE3){
+            if(resultCode == CONF){
+                TextView tv = findViewById(R.id.tv_r3);
+                tv.setText(""+resultado);
+            }
+        }
+
     }
 }
--- app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
+++ app/src/main/java/br/ufrn/eaj/tads/testedaltonismo/TesteActivity.java
@@ -1,13 +1,50 @@
 package br.ufrn.eaj.tads.testedaltonismo;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class TesteActivity extends AppCompatActivity {
-
+    public static final int TESTE1 = 11,TESTE2 = 22, TESTE3 = 33,CONF = 44, CANC = 55;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teste);
+    }
+    public void cliqueOk(View v){
+        EditText et = findViewById(R.id.et_res);
+        Intent i = new Intent();
+        Bundle data = new Bundle();
+        int resultado = Integer.parseInt(et.getText().toString());
+        data.putInt("resultado",resultado);
+        i.putExtras(data);
+        setResult(44,i);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView imv = findViewById(R.id.imv);
+        if(requestCode == RESULT_OK){
+            if(resultCode == TESTE2){
+                imv.setBackgroundResource(R.drawable.teste2);
+                //tv.setText(""+resultado);
+            }
+        }/*else if (requestCode == TESTE2){
+            if(resultCode == CONF){
+                TextView tv = findViewById(R.id.tv_r2);
+                tv.setText(""+resultado);
+            }
+        }else if (requestCode == TESTE3){
+            if(resultCode == CONF){
+                TextView tv = findViewById(R.id.tv_r3);
+                tv.setText(""+resultado);
+            }
+        }*/
     }
 }
